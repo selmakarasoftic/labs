4f55e8a874c0d4beb39059d7835414cbc4ebde52
selma <selma.karasoftic@stu.ibu.edu.ba>
2024-11-18 20:57:41 +0100

    week 6 tasks

---
+++ src/main/java/org/example/week6/Generics/task1.java
@@ -0,0 +1,9 @@
+package src.main.java.org.example.week6.Generics;
+import java.util.*;
+
+public class task1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        sumOddEven.calculateNumberSum(numbers);
+    }
+}
