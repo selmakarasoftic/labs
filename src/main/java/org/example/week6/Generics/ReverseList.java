ee3a7516186b8e28fc2eb0bd808628d93b59f6a2
selma <selma.karasoftic@stu.ibu.edu.ba>
2024-11-18 21:11:50 +0100

    week 6 tasks

---
+++ src/main/java/org/example/week6/Generics/ReverseList.java
@@ -0,0 +1,16 @@
+package src.main.java.org.example.week6.Generics;
+import java.util.*;
+
+public class ReverseList {
+
+    public static < T > List < T > reverseList(List < T > originalList) {
+        List < T > reversedList = new ArrayList < > (originalList);
+    /*
+        for (int i = originalList.size() - 1; i >= 0; i--) {
+            reversedList.add(originalList.get(i));
+        }
+    */
+        Collections.reverse(reversedList);
+        return reversedList;
+    }
+}
